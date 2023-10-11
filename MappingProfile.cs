4c16cafeacaf4a1cb74491336a47f109f3a538a7
TsilaAllaoui <tsila.allaoui@gmail.com>
2023-10-11 11:08:28 +0300

    Added mapper profile

---
+++ MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Pharmacy.Dtos.Medicine;
+using Pharmacy.Models;
+
+namespace Pharmacy
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() {
+            CreateMap<Medicine, GetMedicineDto>();
+            CreateMap<GetMedicineDto, Medicine>();
+            CreateMap<AddMedicineDto, Medicine>();
+        }
+
+    }
+}
